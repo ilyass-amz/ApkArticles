d248049a13fe330df40e954ef7d1e24745178739
AMEZIANE ILYASS <amezianeilyass016@gmail.com>
2026-02-15 00:48:03 +0100

    test

---
+++ app/src/main/java/com/exemple/article/Adapters/ArticleAdapter.java
@@ -0,0 +1,53 @@
+package com.exemple.article.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.exemple.article.Article;
+import com.exemple.article.R;
+
+import java.util.List;
+
+public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder> {
+    private List<Article> articles;
+    public  ArticleAdapter(List<Article> articles){
+        this.articles=articles;
+    }
+
+    @Override
+    public ArticleAdapter.ArticleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_article,parent,false);
+        return new ArticleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder( ArticleAdapter.ArticleViewHolder holder, int position) {
+    Article current=articles.get(position);
+    holder.title.setText(current.getTitle());
+    holder.body.setText(current.getBody());
+    Glide.with(holder.itemView.getContext())
+            .load(current.getImageUrl())
+            .into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
+public static class ArticleViewHolder extends RecyclerView.ViewHolder{
+        TextView title,body;
+        ImageView image;
+        public ArticleViewHolder(View itemView){
+        super(itemView);
+        title=itemView.findViewById(R.id.title);
+        body=itemView.findViewById(R.id.body);
+        image=itemView.findViewById(R.id.imgArticle);
+        }
+}
+}
---
+++ app/src/main/java/com/exemple/article/Ui/Home.java
@@ -0,0 +1,50 @@
+package com.exemple.article.Ui;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.exemple.article.Adapters.ArticleAdapter;
+import com.exemple.article.Article;
+import com.exemple.article.Helpers.GsonHelper;
+import com.exemple.article.Helpers.VolleyHelper;
+import com.exemple.article.R;
+
+import java.util.List;
+
+public class Home extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArticleAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        loadData();
+    }
+    private void loadData(){
+        String url="https://jsonplaceholder.typicode.com/posts";
+        StringRequest request=new StringRequest(Request.Method.GET,url,
+                response->{
+                    List<Article> articles= GsonHelper.parseJson(response);
+                    adapter=new ArticleAdapter(articles);
+                    recyclerView.setAdapter(adapter);
+                },
+                error->{
+                    Toast.makeText(this,"Erreur réseau",Toast.LENGTH_SHORT).show();
+                }
+                );
+        VolleyHelper.getInstance(this).addToRequestQueue(request);
+    }
+}
